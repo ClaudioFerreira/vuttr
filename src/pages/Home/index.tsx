a9584829a4651ea7bf783b37b8c3f96360fe3553
Claudio Ferreira <claudio.zero0@gmail.com>
2020-08-17 21:34:20 -0300

    Start project - VUTTR

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import SimpleCard from '../../components/SimpleCard';
+
+const Home: React.FC = () => {
+
+  return (
+    <React.Fragment>
+      <h1>VUTTR</h1>
+      <h4>Veri Useful Tools to Remember</h4>
+
+      <SimpleCard/>
+    </React.Fragment>
+  )
+}
+
+export default Home;
